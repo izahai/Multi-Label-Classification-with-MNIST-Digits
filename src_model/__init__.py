13d164a74a7e961fa0b0ec5ce53e8945ccc459dc
izahai <ngynthhai@gmail.com>
2026-07-29 22:14:47 +0700

    delete: detection model

--- src_model/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Neural-network architectures for the MNIST detection project."""
-
-from .model_factory import build_detector
-from .mnist_detector import MNISTDetector
-from .small_mnist_detector import SmallMNISTDetector
-from .yolo_mnist_detector import YOLOLargeMNISTDetector, YOLOSmallMNISTDetector
-
-__all__ = ["MNISTDetector", "SmallMNISTDetector", "YOLOSmallMNISTDetector", "YOLOLargeMNISTDetector", "build_detector"]
